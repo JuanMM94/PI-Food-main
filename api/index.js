cecfb9e5fe67297e279bf4315203765b7f2e2890
Juan Martin <juanmartinmonasterio@gmail.com>
2022-11-02 22:30:21 -0300

    preload Recipes

--- api/index.js
+++ api/index.js
@@ -23,11 +23,11 @@ const { conn, Diet, Recipe } = require("./src/db.js");
 const { YOUR_API_KEY } = process.env;
 
 // Syncing all the models at once.
-conn.sync({ force: true }).then(() => {
+conn.sync({ force: false }).then(() => {
   server.listen(3001, () => {
     console.log("%s listening at 3001"); // eslint-disable-line no-console
   });
-  const preload = async () => {
+  (async () => {
     const dietsPreload = [
       { name: "Gluten Free" },
       { name: "Ketogenic" },
@@ -39,18 +39,33 @@ conn.sync({ force: true }).then(() => {
       { name: "Low FODMAP" },
       { name: "Whole30" },
     ];
-    const recipeFindAll = await Recipe.findAll();
-    if (recipeFindAll.length === 0) {
-      const axiosGetAll = await axios.get(
-        `https://api.spoonacular.com/recipes/complexSearch?apiKey=${YOUR_API_KEY}&addRecipeInformation=true&number=1`
+    const recipesCountAll = await Recipe.count();
+    const recipesAmount = 100;
+    const dietsCountAll = await Diet.count();
+    if (dietsCountAll !== 9) {
+      Diet.bulkCreate(dietsPreload)
+        .then(console.log("Diets have been preloaded successfully"))
+        .catch((error) => console.log("Error in Diets preload", error));
+    } else console.log("Diets already exist in the database");
+    if (recipesCountAll !== recipesAmount) {
+      const getAllRecipes = await axios.get(
+        `https://api.spoonacular.com/recipes/complexSearch?apiKey=${YOUR_API_KEY}&addRecipeInformation=true&number=${recipesAmount}`
       );
-      axiosGetAll.data.results.forEach((element) => {
-        return Recipe.findOrCreate(element);
+      const recipesArray = [];
+      getAllRecipes.data.results.forEach((element) => {
+        recipesArray.push({
+          title: element.title,
+          summary: element.summary,
+          healthScore: element.healthScore,
+          steps: element.analyzedInstructions[0]
+            ? JSON.stringify(element.analyzedInstructions[0].steps)
+            : null,
+          image: element.image,
+        });
       });
-    }
-    Diet.bulkCreate(dietsPreload)
-      .then(console.log("Diets have been preloaded succesfully"))
-      .catch((error) => console.log("Error in Diets preload", error));
-  };
-  preload();
+      Recipe.bulkCreate(recipesArray)
+        .then(console.log("Recipes have been preloaded successfully"))
+        .catch((error) => console.log("Error in Recipes preload", error));
+    } else console.log("Recipes already exist in the database");
+  })();
 });
