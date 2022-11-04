e4b01ae3c368caf95b6f991d30fd41a6d8036676
Juan Martin <juanmartinmonasterio@gmail.com>
2022-11-04 02:56:17 -0300

    api/recipe spoonacular -> database

--- api/src/routes/recipes.js
+++ api/src/routes/recipes.js
@@ -1,64 +1,43 @@
-const { Router } = require('express');
+const { Router } = require("express");
 const { Op } = require("sequelize");
-const axios = require('axios').default;
-const { YOUR_API_KEY } = process.env;
+const axios = require("axios").default;
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
-const { Recipe, Diet } = require('../db');
+const { Recipe, Diet } = require("../db");
 const router = Router();
 
 // Configurar los routers
-// Ejemplo: router.use('/auth', authRouter);  
+// Ejemplo: router.use('/auth', authRouter);
 
-router.get('/', async (req, res) => {
+router.get("/", async (req, res) => {
+  const { name } = req.query;
   try {
-    const axiosGetAll = await axios.get(`https://api.spoonacular.com/recipes/complexSearch?apiKey=${YOUR_API_KEY}&addRecipeInformation=true&number=100`);
-    if (req.query.name) {
-      const dbGetTitle = await Recipe.findAll({
-        include: [{
-          model: Diet,
-          attributes: ['name']
-        }],
+    if (name) {
+      const recipesFindByName = await Recipe.findAll({
         where: {
           title: {
-            [Op.iLike]: req.query.name
-          }
-        }
+            [Op.iLike]: `%${name}%`,
+          },
+        },
+        include: [
+          {
+            model: Diet,
+            attributes: ["name"],
+          },
+        ],
       });
-      const axiosGetTitle = axiosGetAll.data.results.filter(el => el.title.toLowerCase().includes(req.query.name));
-      const response = axiosGetTitle.concat(dbGetTitle);
-      if (response.length > 0) res.status(200).send(response);
-      else res.status(404).send(`Recipes containing ${req.query.name} not found.`);
+      res.send(recipesFindByName);
     } else {
-      let dbGetAll = await Recipe.findAll({
-        include: [{
-          model: Diet,
-          attributes: ['name']
-        }]
+      const recipesFindAll = await Recipe.findAll({
+        include: [
+          {
+            model: Diet,
+            attributes: ["name"],
+          },
+        ],
       });
-          
-      const result = Promise.all([axiosGetAll.data.results, dbGetAll]);
-      result.then(([a, b]) => {
-        const response = [...a,...b];
-        res.status(200).send(response);
-      });
-    };
-  } catch (error) {
-    console.log(error);
-    res.sendStatus(404);
-  }
-});
-
-router.get('/:recipeId', async (req, res) => {
-  const { recipeId } = req.params;
-  try {
-    if (recipeId.includes('-')) {
-      const dbGetId = await Recipe.findByPk(recipeId);
-      res.status(200).send(dbGetId);
-    } else {
-      const axiosGetId = await axios.get(`https://api.spoonacular.com/recipes/${recipeId}/information?apiKey=${YOUR_API_KEY}`);
-      res.status(200).send(axiosGetId.data);  
+      res.status(200).send(recipesFindAll);
     }
   } catch (error) {
     console.log(error);
@@ -66,7 +45,18 @@ router.get('/:recipeId', async (req, res) => {
   }
 });
 
-router.post('/create', async (req, res) => {
+router.get("/:recipeId", async (req, res) => {
+  const { recipeId } = req.params;
+  try {
+    const recipeFindId = await Recipe.findByPk(recipeId);
+    res.status(200).send(recipeFindId);
+  } catch (error) {
+    console.log(error);
+    res.sendStatus(404);
+  }
+});
+
+router.post("/create", async (req, res) => {
   const { title, summary, healthScore, steps, image, diets } = req.body;
   try {
     const newRecipe = await Recipe.create({
@@ -74,21 +64,25 @@ router.post('/create', async (req, res) => {
       summary,
       healthScore,
       steps,
-      image
+      image,
     });
     if (diets) {
       const newRecipeDiets = [...diets];
       const newRecipeId = Recipe.findAll({
         where: {
           title: {
-            [Op.eq]: title
-          }
-        }
+            [Op.eq]: title,
+          },
+        },
       });
-      newRecipeId.then(res => {
-        newRecipeDiets.forEach(el => axios.post(`http://localhost:3001/api/recipes/${res[0].dataValues.id}/diets/${el}`))
+      newRecipeId.then((res) => {
+        newRecipeDiets.forEach((element) =>
+          axios.post(
+            `http://localhost:3001/api/recipes/${res[0].dataValues.id}/diets/${element}`
+          )
+        );
       });
-    };
+    }
     res.status(201).json(newRecipe);
   } catch (error) {
     console.log(error);
@@ -96,7 +90,7 @@ router.post('/create', async (req, res) => {
   }
 });
 
-router.post('/:recipeId/diets/:dietsId', async (req, res) => {
+router.post("/:recipeId/diets/:dietsId", async (req, res) => {
   try {
     const { recipeId, dietsId } = req.params;
     const recipe = await Recipe.findByPk(recipeId);
