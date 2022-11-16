c71e64325e6d59ee56d9f4bfb42e45d8d4857168
Juan Martin <juanmartinmonasterio@gmail.com>
2022-11-16 13:57:32 -0300

    railway deploy env

--- api/src/db.js
+++ api/src/db.js
@@ -2,17 +2,17 @@ require("dotenv").config();
 const { Sequelize } = require("sequelize");
 const fs = require("fs");
 const path = require("path");
-const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
+const { PGUSER, PGPASSWORD, PGHOST, PGDATABASE, PGPORT } = process.env;
 
 const sequelize =
   process.env.NODE_ENV === "production"
     ? new Sequelize({
-        database: DB_NAME,
-        dialect: "postgres",
-        host: DB_HOST,
-        port: 5432,
-        username: DB_USER,
-        password: DB_PASSWORD,
+        database: PGDATABASE,
+        dialect: PGUSER,
+        host: PGHOST,
+        port: PGPORT,
+        username: PGUSER,
+        password: PGPASSWORD,
         pool: {
           max: 3,
           min: 1,
@@ -27,7 +27,7 @@ const sequelize =
         },
         ssl: true,
       })
-    : new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`, {
+    : new Sequelize(`postgres://${PGUSER}:${PGPASSWORD}@${PGHOST}/food`, {
         logging: false, // set to console.log to see the raw SQL queries
         native: false, // lets Sequelize know we can use pg-native for ~30% more speed
       });
